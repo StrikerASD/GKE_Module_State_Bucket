c168c9fe206a78118652ae7424e02374c16dcb89
izaleski <iwo.zaleski@payback.net>
2024-07-09 12:48:21 +0200

    Added depends_on to iam member

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "key_ring" {
   value = google_kms_key_ring.my_keyring.name
 }
 
-# output "my_key" {
-#   value = google_kms_crypto_key.my_key.name
-# }
+output "my_key" {
+  value = google_kms_crypto_key.my_key.name
+}
