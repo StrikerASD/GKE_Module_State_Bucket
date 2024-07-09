fa53cee00d598a85b8978e0f5775ac01fd8525f0
izaleski <iwo.zaleski@payback.net>
2024-07-09 08:29:38 +0200

    Added depends_on to iam member

--- main.tf
+++ main.tf
@@ -19,24 +19,24 @@ resource "google_kms_crypto_key_iam_member" "iam_for_crypto_key" {
 #   ]
 }
 
-resource "google_storage_bucket" "terraform_state_bucket" {
-  name     = var.google_storage_bucket_name
-  location = var.google_storage_bucket_location
-
-  versioning {
-    enabled = var.google_storage_bucket_versioning_enabled
-  }
-
-  lifecycle_rule {
-    condition {
-      age = var.google_storage_bucket_lifecycle_rule_condition_age
-    }
-    action {
-      type = var.google_storage_bucket_lifecycle_rule_action_type
-    }
-  }
-
-  encryption {
-    default_kms_key_name = var.google_storage_bucket_encryption_default_kms_key_name
-  }
-}
+# resource "google_storage_bucket" "terraform_state_bucket" {
+#   name     = var.google_storage_bucket_name
+#   location = var.google_storage_bucket_location
+#
+#   versioning {
+#     enabled = var.google_storage_bucket_versioning_enabled
+#   }
+#
+#   lifecycle_rule {
+#     condition {
+#       age = var.google_storage_bucket_lifecycle_rule_condition_age
+#     }
+#     action {
+#       type = var.google_storage_bucket_lifecycle_rule_action_type
+#     }
+#   }
+#
+#   encryption {
+#     default_kms_key_name = var.google_storage_bucket_encryption_default_kms_key_name
+#   }
+# }
