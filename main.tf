66ebb9d9517346faa480c59f686363be9c581be4
izaleski <iwo.zaleski@payback.net>
2024-07-09 12:36:52 +0200

    Added depends_on to iam member

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ resource "google_kms_crypto_key" "my_key" {
   rotation_period = var.google_kms_crypto_key_rotation_period
 }
 
-# resource "google_kms_crypto_key_iam_member" "iam_for_crypto_key" {
-#   crypto_key_id = google_kms_key_ring.my_keyring.id
-#   role          = var.google_kms_crypto_key_iam_member_role
-#   member        = var.google_kms_crypto_key_iam_member_member
-#   depends_on = [
-#     google_kms_crypto_key.my_key
-#   ]
-# }
+resource "google_kms_crypto_key_iam_member" "iam_for_crypto_key" {
+  crypto_key_id = google_kms_crypto_key.my_key.id
+  role          = var.google_kms_crypto_key_iam_member_role
+  member        = var.google_kms_crypto_key_iam_member_member
+  depends_on = [
+    google_kms_crypto_key.my_key
+  ]
+}
 
 # resource "google_storage_bucket" "terraform_state_bucket" {
 #   name     = var.google_storage_bucket_name
